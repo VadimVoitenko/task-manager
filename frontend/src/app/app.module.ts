f38eb66a8d4baae379acfb62dbcb168228942038
Vadim Voitenko <mirvoy8031@gmail.com>
2024-01-27 12:42:37 +0200

    Creating and Dispalying Lists

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NewListComponent } from './pages/new-list/new-list.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskViewComponent],
+  declarations: [AppComponent, TaskViewComponent, NewListComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
